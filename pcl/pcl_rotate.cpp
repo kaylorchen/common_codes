323e299bc19cbfbcfa8e55769d39bb6b00199aa6
kaylorchen <kaylor.chen@qq.com>
2023-09-25 09:50:00 +0800

    Update

--- pcl/pcl_rotate.cpp
+++ pcl/pcl_rotate.cpp
@@ -7,9 +7,9 @@ PclRotate::transformPointCloud(const RobotState &current_state) {
   // 定义旋转角度和旋转轴
   Eigen::Affine3f transform = Eigen::Affine3f::Identity();
   // 平移向量
-  transform.translation() << delta_x, delta_y, 0;
+  transform.translation() << -delta_x, -delta_y, 0;
   // 旋转矩阵
-  transform.rotate(Eigen::AngleAxisf(delta_yaw, Eigen::Vector3f::UnitZ()));
+  transform.rotate(Eigen::AngleAxisf(-delta_yaw, Eigen::Vector3f::UnitZ()));
   // 对点云进行旋转和平移
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_rotated(new pcl::PointCloud<pcl::PointXYZ>);
   pcl::transformPointCloud(*(this->cloud_), *cloud_rotated, transform);
