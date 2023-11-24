afdaf5ddcbe14a5675946f7c35f482e7855196d6
kaylor <kaylor.chen@qq.com>
2023-11-24 10:00:00 +0800

    Update timeout

--- timeout/timeout.h
+++ timeout/timeout.h
@@ -4,22 +4,21 @@
 
 #pragma once
 #include <chrono>
-template<typename T>
 class Timeout {
  public:
-  Timeout(const T &timeout) : timeout_(timeout) {
+  Timeout(const std::chrono::nanoseconds &timeout) : timeout_(timeout) {
     start_ = std::chrono::steady_clock::now();
   }
   bool isTimeout() {
-    auto elapsed = std::chrono::duration_cast<T>(std::chrono::steady_clock::now() - start_);
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - start_);
     if (elapsed >= timeout_) { return true; }
     return false;
   }
-  void set_timeout(const T &timeout) {
+  void set_timeout(const std::chrono::nanoseconds &timeout) {
     timeout_ = timeout;
     start_ = std::chrono::steady_clock::now();
   }
  private:
-  T timeout_;
+  std::chrono::nanoseconds timeout_;
   std::chrono::steady_clock::time_point start_;
 };
