00ee53008446acdfecd375acb14c8522f5da5319
kaylorchen <kaylor.chen@qq.com>
2024-01-17 10:23:11 +0800

    add yaml

---
+++ yaml/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include "yaml-cpp/yaml.h"
+
+template <typename T>
+std::unique_ptr<std::vector<T>> parse_vector(YAML::Node &config,
+                                             std::string &&key) {
+  if (config[key] && config[key].IsSequence()) {
+    auto msg = std::make_unique<std::vector<T>>();
+    for (int i = 0; i < config[key].size(); ++i) {
+      msg->emplace_back(config[key][i].as<T>());
+    }
+    for (const auto &message : *msg) {
+      std::cout << message << std::endl;
+    }
+    return std::move(msg);
+  }
+  return nullptr;
+}
+
+int main() {
+  YAML::Node config = YAML::LoadFile("../config.yaml");
+  if (config["name"]) {
+    std::cout << "Name: " << config["name"].as<std::string>() << std::endl;
+  }
+  parse_vector<int>(config, "numbers");
+  parse_vector<std::string>(config, "strings");
+  return 0;
+}
