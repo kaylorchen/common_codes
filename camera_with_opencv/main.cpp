277c58e5044784291e08b94f25e8c0be5a40bee5
kaylorchen <kaylor.chen@qq.com>
2024-05-06 15:36:03 +0800

    add video mode

--- camera_with_opencv/main.cpp
+++ camera_with_opencv/main.cpp
@@ -7,7 +7,44 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/videoio.hpp>
+void capture_and_save_video(const std::string& device_name, const std::string& save_name) {
+  cv::VideoCapture cap(device_name);  // 使用设备名称打开视频捕获设备
 
+  if (!cap.isOpened()) {
+    std::cerr << "Error: Unable to open camera device " << device_name << std::endl;
+    return;
+  }
+
+  // 获取捕获的帧率和帧尺寸
+  double fps = cap.get(cv::CAP_PROP_FPS);
+  int frameWidth = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_WIDTH));
+  int frameHeight = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_HEIGHT));
+
+  // 初始化VideoWriter对象
+  cv::VideoWriter writer(save_name, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), fps, cv::Size(frameWidth, frameHeight));
+
+  if (!writer.isOpened()) {
+    std::cerr << "Error: Unable to open video writer" << std::endl;
+    return;
+  }
+
+  cv::Mat frame;
+  int frameCount = 0;
+  const int maxFrames = 150;  // 设置最大帧数限制
+
+  while (frameCount < maxFrames) {
+    if (!cap.read(frame)) {  // 从摄像头读取一帧图像到frame中
+      std::cerr << "Error: Unable to capture image from camera device " << device_name << std::endl;
+      break;
+    }
+
+    writer.write(frame);  // 将帧写入视频文件中
+    frameCount++;
+  }
+
+  cap.release(); // 释放VideoCapture对象
+  writer.release(); // 释放VideoWriter对象
+}
 void capture_and_save_image(const std::string &device_name, const std::string &save_name) {
   // 使用设备名打开视频捕获设备
   cv::VideoCapture cap(device_name);
@@ -69,12 +106,10 @@ int main(int argc, char *argv[]) {
   auto current_time = GetCurrentDateTime();
   std::cout << "current time is " << current_time << std::endl;
   for (int i = 0; i < devices.size(); ++i) {
-    capture_and_save_image(devices.at(i), current_time + "_" + std::to_string(i) +".jpg");
+    std::cout << "processing device " << devices[i] << std::endl;
+//    capture_and_save_image(devices.at(i), current_time + "_" + std::to_string(i) +".jpg");
+    capture_and_save_video(devices.at(i), current_time + "_" + std::to_string(i) +".mp4");
   }
-//  for (auto &item: devices) {
-//    std::cout << item << std::endl;
-//    capture_and_save_image(item, current_time + )
-//  }
 
   return 0;
 }
