fdfdc87540aac1f2d0e0e651c3b3db80474909de
kaylorchen <kaylor.chen@qq.com>
2024-05-11 11:19:54 +0800

    Update

--- opencv_reshape/blob.cpp
+++ opencv_reshape/blob.cpp
@@ -9,10 +9,11 @@
 
 using namespace std;
 
-const int width = 640;
-const int height = 640;
+const int width = 6400;
+const int height = 6400;
 
 uint64_t PopulateData(const cv::Mat &data, float *dst) {
+  std::cout << sizeof(*dst) << " " << sizeof(dst) << std::endl;
   if (data.channels() != 3 || data.type() != CV_8UC3) {
     KAYLORDUT_LOG_ERROR("Invalid data");
     return 0;
@@ -21,14 +22,47 @@ uint64_t PopulateData(const cv::Mat &data, float *dst) {
   auto *R = dst;
   auto *G = dst + data.total();
   auto *B = dst + data.total() * 2;
+  auto *src = data.ptr();
+//  for (int i = 0; i < data.total()*data.channels(); i +=3) {
+//    *B = *(src + i);
+//    B++;
+//    *G = *(src + i + 1);
+//    G++;
+//    *R = *(src + i + 2);
+//    R++;
+//  }
+//  for (int i = 0; i < data.total() * data.channels(); ++i) {
+//    *dst = (*dst)/255.0f;
+//    dst++;
+//  }
+//  for (int i = 0; i < data.total()*data.channels(); i +=3) {
+//    *B = *(src + i)/255.0f;
+//    B++;
+//    *G = *(src + i + 1)/255.0f;
+//    G++;
+//    *R = *(src + i + 2)/255.0f;
+//    R++;
+//  }
+//  for (int i = 0; i < data.rows; ++i) {
+//    for (int j = 0; j < data.cols; ++j) {
+//      // Mat 的数据是BGR
+//      *B = data.at<cv::Vec3b>(i, j)[0]/255.0f;
+//      B++;
+//      *G = data.at<cv::Vec3b>(i, j)[1]/255.0f;
+//      G++;
+//      *R = data.at<cv::Vec3b>(i, j)[2]/255.0f;
+//      R++;
+//    }
+//  }
   for (int i = 0; i < data.rows; ++i) {
     for (int j = 0; j < data.cols; ++j) {
       // Mat 的数据是BGR
-      *B = data.at<cv::Vec3b>(i, j)[0]/255.0f;
+      auto tmp = data.ptr<cv::Vec3b>(i)[j];
+      *B = tmp[0] / 255.0f;
       B++;
-      *G = data.at<cv::Vec3b>(i, j)[1]/255.0f;
+      *G = tmp[1] / 255.0f;
       G++;
-      *R = data.at<cv::Vec3b>(i, j)[3]/255.0f;
+      *R = tmp[2] / 255.0f;
       R++;
     }
   }
@@ -43,17 +77,18 @@ int main(int argc, char *argv[]) {
   std::cout << A.elemSize() << "\n";
   std::cout << A.total() << "\n";
   std::cout << A.channels() << std::endl;
-  for (int i = 0; i < A.rows; i++) {
-    for (int j = 0; j < A.cols; ++j) {
-      A.at<cv::Vec3b>(i, j) = cv::Vec3b(0, 128, 255);
-    }
-  }
+  cv::randu(A,cv::Scalar(0,0,0), cv::Scalar (255,255,255));
+//  for (int i = 0; i < A.rows; i++) {
+//    for (int j = 0; j < A.cols; ++j) {
+//      A.at<cv::Vec3b>(i, j) = cv::Vec3b(0, 128, 255);
+//    }
+//  }
 //  std::cout << A << std::endl;
 
   cv::Mat B;
   KAYLORDUT_TIME_COST_INFO(
       "cv::dnn::blobFromImage",
-      cv::dnn::blobFromImage(A, B, 1 / 255.0f, cv::Size(), cv::Scalar(0, 0, 0), false, false, CV_32F);
+      cv::dnn::blobFromImage(A, B, 1 / 255.0f, cv::Size(), cv::Scalar(0, 0, 0), true, false, CV_32F);
   );
 //  cout << "Blob shape: " << B.size[0] << "x" << B.size[1] << "x" << B.size[2] << "x" << B.size[3] << endl;
 //  int N = B.size[0];  // 批大小
@@ -77,7 +112,7 @@ int main(int argc, char *argv[]) {
 //      }
 //    }
 //  }
-//  auto *ptr = (float *) (B.ptr());
+  auto *ptr = (float *) (B.ptr());
 //  for (int i = 0; i < A.rows; ++i) {
 //    for (int j = 0; j < A.cols; ++j) {
 //      for (int k = 0; k < 3; ++k) {
@@ -87,7 +122,7 @@ int main(int argc, char *argv[]) {
 //    }
 //    std::cout << std::endl;
 //  }
-
+//
 
   auto result = std::make_unique<float[]>(A.total() * A.channels());
   KAYLORDUT_TIME_COST_INFO(
